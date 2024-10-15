946c5ac1957b152db2a1f0f0c74b49306bcf0199
Charlie-Gauvin <gauvinch9@gmail.com>
2024-10-15 15:03:46 +0200

    Création component project

---
+++ src/components/project/Project.tsx
@@ -0,0 +1,19 @@
+function Project() {
+  return (
+    <section
+      id="projects"
+      className="mt-40 flex flex-col items-center pt-16 text-center"
+    >
+      <div className="pb-16">
+        <h3 className="mb-4 font-syne text-3xl font-normal text-primary sm:text-5xl md:text-6xl">
+          My Projects
+        </h3>
+        <p className="font-rubik text-xl font-normal text-orange sm:text-2xl md:text-3xl">
+          Crafted with Passion
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default Project;
